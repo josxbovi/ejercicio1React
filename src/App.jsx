0d2cec7fd300920cf2614ddb6a09c5d0c0153432
José Eugenio Navarro Bovi <jose.navarrobovi@alu.frt.utn.edu.ar>
2024-02-04 03:41:32 -0300

    Primer commit y ejercicio terminado

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Ejercicio1 from './components/Ejercicio1';
+
+function App() {
+  return(
+    <>
+    <h1 className='my-5 h1'>Ejercicios React</h1>
+    <Ejercicio1 />
+    </>
+  )
+}
+export default App
---
+++ src/components/Ejercicio1.jsx
@@ -0,0 +1,11 @@
+const Ejercicio1 = ()  => {
+    return (
+        <article>
+            <h3 className="display-2 text-center">
+                Hola Mundo
+            </h3>
+        </article>
+    );
+};
+
+export default Ejercicio1;
